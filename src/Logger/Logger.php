460f1604d02f403e269693820ebd6ad0be730df7
gaurav <gaurav.gupta1@wingify.com>
2019-07-22 19:36:48 +0530

    chnges for logger

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -4,20 +4,20 @@ use Monolog\Logger as Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Formatter\LineFormatter;
 /**
- * 
+ *
  */
 class Loggers implements LoggerInterface
 {
-	
-	function __construct($argument)
-	{
-		# code...
-		// credentisla 
+
+    function __construct($argument)
+    {
+        # code...
+        // credentisla
 
 
-	}
-	function addLog($msg, $level){
+    }
+    function addLog($msg, $level){
         error_log($msg);
-	}
+    }
 }
 
